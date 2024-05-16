3a78451655e21f3cfcc77e4b056d1d6163864187
Jinho Kim <jkimkj@gmail.com>
2024-05-16 13:04:39 -0700

    fixing errors in split reg emf

--- qc/2p_data/online motion correction/split_reg_emf.py
+++ qc/2p_data/online motion correction/split_reg_emf.py
@@ -10,6 +10,7 @@ from dask import delayed, compute
 from dask.distributed import Client
 import numpy as np
 
+
 def create_plane_h5_from_tiff(data_fn, plane_ind, num_pages=None, num_planes=8, rerun=False):
     base_name = data_fn.name.split('.')[0]
     save_fn = data_fn.parent / f'{base_name}_{plane_ind:02}.h5'
@@ -30,64 +31,86 @@ def create_plane_h5_from_tiff(data_fn, plane_ind, num_pages=None, num_planes=8, 
     return save_fn
 
 
-def register_plane_and_save_emf(h5_fn, frame_rate=11, epoch_minutes=1, key='data'):
-    # suite2p options
-    t0 = time.time()
-    base_name = h5_fn.name.split('.')[0]
-    print(f'{base_name} suite2p registration running...')
-    ops=default_ops()
-    ops['batch_size'] = 1000
-    ops['maxregshift'] = 0.2
-    ops['snr_thresh'] = 1.2 # Default: 1.2 # if any nonrigid block is below this threshold, it gets smoothed until above this threshold. 1.0 results in no smoothing
-    ops['block_size'] = [64, 64]
-    ops['maxregshiftNR'] = np.round(ops['block_size'][0]/10) # Default = 5
-
-    # Read data
-    with h5py.File(h5_fn, 'r') as h5:
-        imgs = h5[key][:]
-    reg_movie = np.zeros_like(imgs)
-
-    # Register
-    register_result = register.compute_reference_and_register_frames(f_align_in=imgs, f_align_out=reg_movie, ops=ops)
-    ops['reg_result'] = register_result
-
-    t1 = time.time()
-    print(f'{base_name} registration done in {(t1-t0)/60:.2f} min.')
-
-    # Save registration results
-    temp_fn_base = h5_fn.name.split('.')[0]
-    save_fn_h5 = h5_fn.parent / f'{temp_fn_base}_reg.h5'
-    save_fn_npy = h5_fn.parent / f'{temp_fn_base}_ops.npy'
-    with h5py.File(save_fn_h5, 'w') as h:
-        h.create_dataset(name='data', data=reg_movie)
-    np.save(save_fn_npy, ops)
-    
-    t2 = time.time()
-    print(f'{base_name} registration saved in {(t2-t1)/60:.2f} min.')
-
-    # calculating and saving episodic mean FOVs (EMF)
-    num_frames = imgs.shape[0]
-    epoch_length = np.round(frame_rate * 60 * epoch_minutes)
-    num_epochs = num_frames // epoch_length
+def _create_and_save_emf(reg_movie, epoch_fn, frame_rate, epoch_minutes):
+    num_frames = reg_movie.shape[0]
+    epoch_length = int(np.round(frame_rate * 60 * epoch_minutes))
+    num_epochs = int(num_frames // epoch_length)
     last_epoch_minutes = epoch_minutes
     if num_frames % epoch_length > epoch_length / 2:
         num_epochs += 1
         last_epoch_minutes = epoch_minutes * ((num_frames % epoch_length) / epoch_length)
-    emf = np.zeros((num_epochs, *imgs.shape[1:]))
+    emf = np.zeros((num_epochs, *reg_movie.shape[1:]))
     for ei in range(num_epochs):
         num_frame_start = ei*epoch_length
         num_frame_end = min((ei+1)*epoch_length, num_frames)
-        emf[ei] = np.mean(reg_movie[num_frame_start:num_frame_end])
-    epoch_fn = h5_fn.parent / f'{temp_fn_base}_emf.h5'   
+        emf[ei] = np.mean(reg_movie[num_frame_start:num_frame_end], axis=0)    
     with h5py.File(epoch_fn, 'w') as h:
         h.create_dataset(name='data', data=emf)
         h.create_dataset(name='num_epochs', data=num_epochs)
         h.create_dataset(name='epoch_minutes', data=epoch_minutes)
         h.create_dataset(name='last_epoch_minutes', data=last_epoch_minutes)
+    epoch_base_fn = epoch_fn.name.split('.')[0]
+    epoch_tif_fn = epoch_fn.parent / f'{epoch_base_fn}.tif'
+    tifffile.imwrite(epoch_tif_fn, emf)
+
+
+def register_plane_and_save_emf(h5_fn, frame_rate=11, epoch_minutes=1, key='data'):
+    t0 = time.time()
+    base_name = h5_fn.name.split('.')[0]
+    save_fn_h5 = h5_fn.parent / f'{base_name}_reg.h5'
+    save_fn_npy = h5_fn.parent / f'{base_name}_ops.npy'
+    epoch_fn = h5_fn.parent / f'{base_name}_emf.h5'
+
+    if save_fn_h5.exists() and save_fn_npy.exists() and epoch_fn.exists():
+        print(f'{base_name} already processed. Quit.')
+
+    elif save_fn_h5.exists() and save_fn_npy.exists():
+        print(f'{base_name} registration already done. Processing episodic mean FOV...')
+        with h5py.File(save_fn_h5, 'r') as h5:
+            reg_movie = h5[key][:]
+        _create_and_save_emf(reg_movie, epoch_fn, frame_rate, epoch_minutes)
+
+        t1 = time.time()
+        print(f'{base_name} EMF saved and done in {(t1-t0)/60:.2f} min.')
     
-    t3 = time.time()
-    print(f'{base_name} EMF saved in {(t3-t2)/60:.2f} min.')
-    print(f'{base_name} done in {(t3-t0)/60:.2f} min')
+    else:
+        # suite2p options
+        print(f'{base_name} suite2p registration running...')
+        ops=default_ops()
+        ops['batch_size'] = 1000
+        ops['maxregshift'] = 0.2
+        ops['snr_thresh'] = 1.2 # Default: 1.2 # if any nonrigid block is below this threshold, it gets smoothed until above this threshold. 1.0 results in no smoothing
+        ops['block_size'] = [64, 64]
+        ops['maxregshiftNR'] = np.round(ops['block_size'][0]/10) # Default = 5
+
+        # Read data
+        with h5py.File(h5_fn, 'r') as h5:
+            imgs = h5[key][:]
+        reg_movie = np.zeros_like(imgs)
+
+        # Register
+        register_result = register.compute_reference_and_register_frames(f_align_in=imgs, f_align_out=reg_movie, ops=ops)
+        ops['reg_result'] = register_result
+
+        t1 = time.time()
+        print(f'{base_name} registration done in {(t1-t0)/60:.2f} min.')
+
+        # Save registration results
+        save_fn_h5 = h5_fn.parent / f'{base_name}_reg.h5'
+        save_fn_npy = h5_fn.parent / f'{base_name}_ops.npy'
+        with h5py.File(save_fn_h5, 'w') as h:
+            h.create_dataset(name='data', data=reg_movie)
+        np.save(save_fn_npy, ops)
+        
+        t2 = time.time()
+        print(f'{base_name} registration saved in {(t2-t1)/60:.2f} min.')
+
+        # calculating and saving episodic mean FOVs (EMF)
+        _create_and_save_emf(reg_movie, epoch_fn, frame_rate, epoch_minutes)
+        
+        t3 = time.time()
+        print(f'{base_name} EMF saved in {(t3-t2)/60:.2f} min.')
+        print(f'{base_name} done in {(t3-t0)/60:.2f} min')
 
 
 def _extract_dict_from_si_string(string):
@@ -113,10 +136,11 @@ def _extract_dict_from_si_string(string):
 
 
 if __name__ == '__main__':
+    num_threads = 10
     num_planes = 8
     epoch_minutes = 1
     data_dir = Path(r'\\allen\programs\mindscope\workgroups\learning\pilots\online motion correction\mouse_721291\test_240509')
-    data_fn = data_dir / 'global_motioncorrection_1x8_00003.tif'
+    data_fn = data_dir / 'no_motioncorrection_1x8_00001.tif'
 
 
     with ScanImageTiffReader(str(data_fn)) as reader:
@@ -137,7 +161,7 @@ if __name__ == '__main__':
     client = Client()
 
     tasks = [delayed(create_plane_h5_from_tiff)(data_fn, pi, num_pages, num_planes) for pi in range(num_planes)]
-    results = compute(*tasks)
+    results = compute(*tasks, num_workers=num_threads)
 
     tasks = [delayed(register_plane_and_save_emf)(h5_fn, frame_rate, epoch_minutes) for h5_fn in results]
-    compute(*tasks)
+    compute(*tasks, num_workers=num_threads)
